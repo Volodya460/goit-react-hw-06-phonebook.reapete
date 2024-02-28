6320329d3816b3826b70a5594cea7b6f675d7547
Volodya Kushch <110915611+Volodya460@users.noreply.github.com>
2024-02-28 17:33:24 +0200

    reapet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { ContactList } from './ContactList/ContactList';
 import Form from './Form/Form';
 import { Section } from './section';
 import { Filter } from './Filter/Filter';
-import { useLocalStorage } from '../Hooks/useLocalStorage';
+// import { useLocalStorage } from '../Hooks/useLocalStorage';
 
 export default function App() {
   return (
